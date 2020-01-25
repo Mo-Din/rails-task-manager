89c65de3e6dc5e124690c318007a0b65ce4294b6
Morgane Dinse <morgane.dinse@gmail.com>
2020-01-25 13:29:17 -0500

    CRUD definition done in my controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,42 @@
 class TasksController < ApplicationController
 
+before_action :set_params, only [:show, :edit, :update, :destroy]
+
+  def index         # GET /tasks
+    @task = Task.all
+  end
+
+  def show          # GET /tasks/:id
+  end
+
+  def new           # GET /tasks/new
+    @task = Task.new
+  end
+
+  def create        # POST /tasks
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit          # GET /tasks/:id/edit
+  end
+
+  def update        # PATCH /tasks/:id
+    @task.update(task_params)
+    redirect_to tasks_path(task)
+  end
+
+  def destroy       # DELETE /tasks/:id
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details,:completed)
+  end
+
+  def set_params
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # READ all tasks
+  get    'tasks',          to: 'tasks#index'
+
+  # READ one task
+  get    "tasks/:id",      to: 'tasks#show'
+
+  # CREATE a task
+  get    'tasks/new',      to: 'tasks#new'
+  post   'tasks',          to: 'tasks#create'
+
+  # UPDATE one task
+  get    'tasks/:id/edit', to: 'tasks#edit'
+  patch  'tasks/:id',      to: 'tasks#update'
+
+  # DELETE a task
+  delete 'tasks/:id',      to: 'tasks#destroy'
 end
